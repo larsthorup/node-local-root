77cf445dfd761c866e59acba40db8d2800073fbb
Lars Thorup <lars@zealake.com>
2016-06-13 21:51:54 +0200

    0.1.0

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # node-local-root
+
+Simplify loading assets from local modules
+
+## Usage
+
+    npm install @larsthorup/root --save
+
+    var root = require('@larsthorup/root');
---
+++ index.js
@@ -0,0 +1,6 @@
+var path = require('path');
+
+// Note: we assume that @larsthorup/root is installed from package.json in the root folder
+var root = path.join(__dirname, '../../..');
+
+module.exports = root;
