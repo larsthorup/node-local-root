bb81f8a3a4a5f3aea6707b6ceddc61706dcfcfb5
Lars Thorup <lars@zealake.com>
2016-11-11 18:15:27 +0100

    support zeit.co/now deploys

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var path = require('path');
 
+// Note: with npm, this module will be in node_modules/@larsthorup/root
+// Note: with zeit.co/now, this module will be in node_modules/<random-id>
+var rootInstallPath = path.basename(__dirname) === 'root' ? '../..' : '..';
+
 // Note: we assume that @larsthorup/root is installed from package.json in the root folder
-var root = path.join(__dirname, '../../..');
+var root = path.join(__dirname, '..', rootInstallPath);
 
 module.exports = root;
